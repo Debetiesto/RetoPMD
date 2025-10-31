e5b8ab00e361b6d91e4be2a652bb5894f6b91713
Debetiesto <luisvazquez8942@gmail.com>
2025-10-31 09:37:37 +0100

    Animación de game over arreglada e implementada

---
+++ Assets/IniciarJuego.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IniciarJuego : MonoBehaviour
+{
+
+    public void abrirJuego()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
---
+++ Assets/Salir.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Salir : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+   public void SalirJuego()
+    {
+        Application.Quit();
+            
+    }
+}
