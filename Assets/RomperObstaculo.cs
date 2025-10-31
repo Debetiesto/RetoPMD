59274b250dcd08a845b1e1c5636cb2d6552adc47
Debetiesto <luisvazquez8942@gmail.com>
2025-10-31 13:01:47 +0100

    Ventana de victoria agregada y configurada y animaciones de coches destruyendose

--- Assets/RomperObstaculo.cs
+++ Assets/RomperObstaculo.cs
@@ -2,12 +2,18 @@ using UnityEngine;
 
 public class RomperObstaculo : MonoBehaviour
 {
+    public GameObject explosionPrefab;
     private void OnCollisionEnter2D(Collision2D collision)
     {
 
         // Verifica si el objeto que colisionó tiene la etiqueta "Pelota"
         if (collision.gameObject.CompareTag("Pelota"))
         {
+            // Crear explosión en la posición del coche
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+
+            ControlObstaculos.CocheDestruido();
+
             // Destruye este bloque
             Destroy(gameObject);
         }
